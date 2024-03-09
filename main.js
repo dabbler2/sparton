e7b46354ba61c609db19ac5831798eb9cf4f4c5b
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2024-03-09 23:09:10 +0900

    .

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const createForm = document.querySelector('form')
 createForm.addEventListener('submit', e => {
 	e.preventDefault()
 	const title = document.getElementById('card_content').value
-	localStorage.setItem(title,0)
+	localStorage.setItem(title,2*(1+Math.floor(Math.random()*6)))
 	alert('카드 추가 완료')
 	location.reload()
 })
@@ -11,10 +11,11 @@ const cards = document.getElementById('cards')
 for(let i=0;i<localStorage.length;++i){
 	const card = document.createElement('div')
 	const title = localStorage.key(i)
+	const value = +localStorage.getItem(title)
 	card.classList.add('bucket')
-	card.classList.add('img3')
+	card.classList.add('img'+(value/2|0))
 	card.classList.add('center')
-	if(+localStorage.getItem(title)) card.classList.add('done')
+	if(value&1) card.classList.add('done')
 	card.innerHTML = title
 	const removeButton = document.createElement('button')
 	removeButton.innerHTML = 'X'
@@ -28,10 +29,9 @@ for(let i=0;i<localStorage.length;++i){
 	card.addEventListener('click', e => {
 		if(e.target.tagName==='DIV'){
 			card.classList.toggle('done')
-			localStorage.setItem(title,1-(+localStorage.getItem(title)))
+			localStorage.setItem(title,(+localStorage.getItem(title))^1)
 		}
 	})
 	card.appendChild(removeButton)
-	
 	cards.appendChild(card)
 }
